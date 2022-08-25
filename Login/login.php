9a9aa2dc1898420ddc854c6b31cb9009e7361cee
Emil MitroYT <emilvenemies@gmail.com>
2022-08-25 10:23:23 +0300

    .

---
+++ Login/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>login</title>
+</head>
+<body>
+	<div class="login">
+		<form action="../player/player.php" method="post">
+			<input method="post" type="text" name="Username" placeholder="Username" id="Username">
+			<input method="post" type="text" name="Password" placeholder="Password" id="Password">
+			<input type="submit" value="Login" />
+		</form>
+	</div>
+
+
+
+</body>
+</html>
