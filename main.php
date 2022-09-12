dfaaf83245a711b8c4175b7369eb5d84f87050f4
emilmitroYT <emilvenemies@gmail.com>
2022-09-12 11:12:38 +0300

    .

--- main.php
+++ main.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="play.css">
-    <title>Main</title>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="play.css" />
+	<script src="play.js"></script>
+    <title>Audio Player</title>
 </head>
 <body>
-<div class="header">
-	<h1>player</h1>
-</div>  
+    <!--link till upload files-->
+    <form name="audio link" action="upload.php">
+    <input type="submit" value="Upload"/>
+    </form>
 
 </body>
 </html>
---
+++ upload.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles/play.css" />
+	<script src="play.js"></script>
+    <title>upload</title>
+</head>
+<body>
+    <h1>under maintenance</h1>
+    <form name="audio_form" id="audio_form" action="main.php" method="post" enctype="multipart/form-data">
+    <label>Audio File:</label>
+    <input name="audio_file" id="audio_file" type="file"/>
+    <input type="submit" name="Submit" id="Submit" value="Submit"/>
+    </form>
+
+</body>
+</html>
